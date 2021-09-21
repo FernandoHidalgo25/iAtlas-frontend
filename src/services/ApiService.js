7663a3592ae604a782e32ef407da8a750d77b168
github username <github email address>
2021-09-21 10:02:18 -0400

    a

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,29 +1,27 @@
 class ApiService {
 
-    constructor(api){
-      this.api = api
-    }
-  
-    getCountries = () => fetch(this.api + "/countries").then(res => res.json())
+  constructor(api){
+    this.api = api
+  }
 
-    createCountry = (newCountry) => {
-      return fetch(this.api + "/countries", {
-          method: 'POST', // or 'PUT'
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newCountry),
-        })
-        .then(response => response.json())
-    }
-    findOrCreateUser = (username) => {
-      return fetch(this.api + "/users", {
+  getCountries = () => fetch(this.api + "/countries").then(res => res.json())
+
+  createCountry = (newCountry) => fetch(this.api + "/countries", {
         method: 'POST', // or 'PUT'
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({username: username}),
+        body: JSON.stringify(newCountry),
       })
       .then(response => response.json())
-    }
-}
+  }
+  findOrCreateUser = (username) => {
+    return fetch(this.api + "/users", {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username: username}),
+    })
+    .then(response => response.json())
+  }
